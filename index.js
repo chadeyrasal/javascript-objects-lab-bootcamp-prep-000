124627e691cc9febb9765ae652f486a3e4eaefc1
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-11 15:59:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var recipes = {prop: '1'}
 
-var firstKey
-var firstValue
+var firstKey = 'prop2'
+var firstValue = 2
 
 function updateObjectWithKeyAndValue(recipes, firstKey, firstValue) {
-  firstKey = 'prop2'
-  firstValue = 2
   recipes.firstKey = firstValue
   return recipes
 }
