ff3f555dddaa8d6423247af8db7591c19f386b62
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-11 16:02:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var recipes = {prop: '1'}
 var prop2 = 'prop2'
 var value = '2'
 
-function updateObjectWithKeyAndValue(recipes, prop2, value) {
-  recipes.prop2 = value
-  return recipes
+function updateObjectWithKeyAndValue() {
+  
 }
